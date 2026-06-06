f408b8e2a09bebf7b8d040abc24e012377e9c638
Karen Cuno Toledo <k.cynthia.c@gmail.com>
2026-06-05 23:39:47 -0500

    Corregir codificacion procesamiento

--- scripts/procesamiento.py
+++ scripts/procesamiento.py
@@ -1,41 +1,11 @@
 import pandas as pd
 import psycopg2
 
-# Leer CSV
 df = pd.read_csv("input/dataset.csv")
 
-# Conexión PostgreSQL
 conexion = psycopg2.connect(
     host="localhost",
     dbname="ventasdb",
     user="admin",
     password="admin123"
 )
-
-cursor = conexion.cursor()
-
-# Crear tabla
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS clientes(
-    id SERIAL PRIMARY KEY,
-    nombre VARCHAR(50),
-    edad INTEGER,
-    ciudad VARCHAR(50)
-)
-""")
-
-# Insertar datos
-for _, fila in df.iterrows():
-    cursor.execute("""
-    INSERT INTO clientes(nombre, edad, ciudad)
-    VALUES (%s,%s,%s)
-    """,
-    (fila['nombre'], fila['edad'], fila['ciudad'])
-    )
-
-conexion.commit()
-
-print("Datos cargados correctamente")
-
-cursor.close()
-conexion.close()
